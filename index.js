6abb176c468741a23c81733d2e9ce052f9e054df
Kazi Minhaj <kaziminhajulhaider@gmail.com>
2021-04-18 00:45:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("server working");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fzhcz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const appointmentCollection = client
+    .db("doctorsPortal")
+    .collection("appointments");
+
+  app.post("/addAppointment", (req, res) => {
+    const appointment = req.body;
+    console.log(appointment);
+    appointmentCollection.insertOne(appointment).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+});
+
+app.listen(process.env.PORT || port);
